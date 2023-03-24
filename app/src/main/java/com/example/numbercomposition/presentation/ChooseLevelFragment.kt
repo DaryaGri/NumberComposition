ec658640907e224b9d1ff6e79e4ce62a93816802
Your Name <daryagrichenok@gmail.com>
2023-03-24 13:52:03 +0300

    Add Navigation Components with safeArgs

--- app/src/main/java/com/example/numbercomposition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/numbercomposition/presentation/ChooseLevelFragment.kt
@@ -5,9 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.numbercomposition.R
+import androidx.navigation.fragment.findNavController
 import com.example.numbercomposition.databinding.FragmentChooseLevelBinding
-import com.example.numbercomposition.databinding.FragmentWelcomeBinding
 import com.example.numbercomposition.domain.entity.Level
 
 class ChooseLevelFragment : Fragment() {
@@ -42,18 +41,8 @@ class ChooseLevelFragment : Fragment() {
     }
 
     private fun launchGameFragment(level: Level) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFragment.newInstance(level))
-            .addToBackStack(GameFragment.NAME)
-            .commit()
-    }
-
-    companion object {
-
-        const val NAME = "ChooseLevelFragment"
-        fun newInstance(): ChooseLevelFragment {
-            return ChooseLevelFragment()
-        }
+        findNavController().navigate(
+            ChooseLevelFragmentDirections.actionChooseLevelFragmentToGameFragment(level))
     }
 
     override fun onDestroyView() {
